bbb7e446167a8fa71d136aaa4a2dbcf825f462d3
Patrick <patricktsaikl@gmail.com>
2024-04-14 11:58:16 +0800

    feat/server: create People type

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,30 @@
-import { BookType } from "../types/index";
+import { BookType, PeopleType } from "../types/index";
+import {
+  fromArrWithKeyToMap,
+  getRandomNFromArr,
+  intToPId,
+} from "../utils/general";
 
-const N = 20;
+const BOOK_COUNT = 20;
+const PEOPLE_COUNT = 40;
+const people: PeopleType[] = new Array(PEOPLE_COUNT).fill(0).map((_, ind) => {
+  return {
+    id: intToPId(ind),
+    name: `user${ind}`,
+    books: [],
+  };
+});
 
-const books: BookType[] = new Array(N).fill(0).map((_, ind) => ({
+const peopleMap: Map<String, PeopleType> = fromArrWithKeyToMap(people, "id");
+
+const books: BookType[] = new Array(BOOK_COUNT).fill(0).map((_, ind) => ({
   id: ind,
   title: `Book ${ind}`,
-  author: `Author ${ind}`,
+  author: people[ind],
+  recommendList: getRandomNFromArr(
+    people,
+    Math.floor(Math.random() * PEOPLE_COUNT)
+  ),
 }));
 
-export { books };
+export { books, people, peopleMap };
